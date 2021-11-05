8612ea15274951ebd53d8beb50ce2efd90fdd6f2
IsaacGomez10 <isaac.21.10@hotmail.com>
2021-11-05 13:51:40 -0500

    Funciones, detalles, en css y js

--- JavaScript/Funciones.js
+++ JavaScript/Funciones.js
@@ -1,29 +1,65 @@
-function openNav(){
+/*-responsive-*/
+function openNav() {
     document.getElementById("celular").style.width = "90%";
 
 }
 
-function closeNav(){
+function closeNav() {
     document.getElementById("celular").style.width = "0";
 }
-
-function innerHTML(id,result){
-    return getElementById(id).innerHTML = result;
-}
-function convertir(tipo){
+/*-modificar palabra-*/
+function convertir(tipo) {
 
     var palabra = document.getElementById("palabra");
     var texto = palabra.value;
 
-    if(tipo == 1){
-        palabra.value = texto.length;
-        return texto.getElementById("vista");
-    }else if(tipo == 2){
+    if (tipo == 1) {
+        var leng = texto.length;
+        vista = document.getElementById("vista").innerHTML = ("La palabra tiene " + leng + " caracteres");
+    } else if (tipo == 2) {
         palabra.value = texto.toUpperCase();
-    }else if (tipo == 3){
+    } else if (tipo == 3) {
         palabra.value = texto.toLowerCase();
-    }else if (tipo = 4){
-        palabra.map(p => p[0].toUpperCase() + p.slice(1)).join(' ');
+    } else if (tipo = 4) {
+        palabra.value = texto.slice(1, 1);
+        vista = document.getElementById("vista").innerHTML = ("Primer caracter es " + palabra.value);
+
+    }
+
+}
+
+/*-calculadora básica-*/
+
+function limpiar() {
+    document.getElementById("formulario").reset();
+}
+
+function resultado() {
+
+    var x = document.getElementById("numero1").value;
+    x = parseInt(x)
+    var y = document.getElementById("numero2").value;
+    y = parseInt(y)
+    var resultado = document.getElementById("operacion").value;
+
+    switch (resultado) {
+        case "1":
+            var suma = (x + y);
+            vista2 = document.getElementById("vista2").innerHTML = ("su resultado es: " + suma);
+            break
+
+        case "2":
+            var resta = (x - y);
+            vista2 = document.getElementById("vista2").innerHTML = ("su resultado es: " + resta);
+            break
+        case "3":
+            var multiplicacion = (x * y);
+            vista2 = document.getElementById("vista2").innerHTML = ("su resultado es: " + multiplicacion);
+            break
+        case "4":
+            var divicion = (x / y);
+            vista2 = document.getElementById("vista2").innerHTML = ("su resultado es: " + divicion);
+            break
     }
 
 }
